107f5d122783a3f34ff6824fd1051e0368bc66d5
Apisitkitti <apisit.o@mail.kmutt.ac.th>
2022-09-03 17:21:58 +0700

    3

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+public class Program_3
+{
+   public static void Main(String[] args)
+    {
+        string NUm_input = Pow_num();
+        int Num_num = StringchangetoInt(NUm_input);
+        Console.WriteLine(Pow_timewe(Num_num));
+
+        
+
+    }
+    public static string Pow_num ()
+    {
+        Console.Write("Pls input your pow number: ");
+        return Console.ReadLine();
+
+    }
+    public static int StringchangetoInt(string Value)
+    {
+        if(int.TryParse(Value,out int Num_changer))
+        {
+            return Num_changer;
+        }
+        throw new Exception("Input again");
+    }
+    public static int Pow_timewe(int num)
+    {   
+        if(Pow_Check(num))
+        {
+            num--;
+          return 2*Pow_timewe(num);
+        }
+        else return 1;      
+    }   
+    public  static bool Pow_Check(int check)
+    {
+        return check > 0; 
+    }
+    public static bool Pow_Check_1(int check)
+    {
+        return check == 1;
+    }
+    
+   
+        
+    
+   
+  
+    
+    
+
+}
+    
